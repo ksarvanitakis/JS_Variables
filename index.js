8ac4f2e0f72f4c11f24cf5bdee5b8a989ef1234d
Konstantinos Arvanitakis <ksarvanitakis@hotmail.com>
2024-01-15 16:44:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+greeting = "Hallo World!";
+
+console.log(greeting);
+
+age = 42;
+
+console.log("I am " + age + " years old!");
+
+
+const name = "Kostas";
+const lastName = "Arvanitakis";
+
+console.log("And my name is " + name);
+
+alert("Thank you Kostas")
+console.log(lastName)
+let a = 10;
+a = a - 4;
+a = 7;
+a = a + 2;
+
+console.log(a);
+
+alert("This is a message for the ones that didnt believe in me!")
+prompt("And now I will prove my self for good!")
+console.log("All done!")
